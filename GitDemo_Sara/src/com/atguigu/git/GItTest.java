e67f865a181a0a3d19822354cd5b2c160b7e89e7
saraxiang <saraxiang@qq.com>
2019-12-19 17:04:28 +0800

    push all

---
+++ GitDemo_Sara/src/com/atguigu/git/GItTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+public class GItTest {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 3; i++) {
+			
+			System.out.println("i love git");
+		}
+		
+	}
+}
